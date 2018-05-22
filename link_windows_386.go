e14586e033f5e15a24e652c2c87d4ee3994896b0
lin.tian <lin.tian@yottabyte.cn>
2018-05-22 14:00:03 +0800

    modify LDFLAGS for mingw in win7 32

--- link_windows_386.go
+++ link_windows_386.go
@@ -2,9 +2,9 @@
 
 package ui
 
-// #cgo LDFLAGS: ${SRCDIR}/libui_windows_386.a ${SRCDIR}/libui_windows_386.res.o
+// #cgo LDFLAGS:  ${SRCDIR}/libui_windows_386.a ${SRCDIR}/libui_windows_386.res.o ${SRCDIR}/libwinapi_d2d1.a ${SRCDIR}/libwinapi_dwrite.a ${SRCDIR}/libwinapi_comctl32.a
 // /* note the order; also note the lack of uuid */
-// #cgo LDFLAGS: -luser32 -lkernel32 -lusp10 -lgdi32 -lcomctl32 -luxtheme -lmsimg32 -lcomdlg32 -ld2d1 -ldwrite -lole32 -loleaut32 -loleacc -static -static-libgcc -static-libstdc++
+// #cgo LDFLAGS: -luser32 -lkernel32 -lusp10 -lgdi32  -luxtheme -lmsimg32 -lcomdlg32 -lole32 -loleaut32 -loleacc -static -static-libgcc -static-libstdc++
 import "C"
 
 func ensureMainThread() {
